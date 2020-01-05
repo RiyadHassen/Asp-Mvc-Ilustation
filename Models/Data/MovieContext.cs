34a0355220610dbdb70a2c40e8e9952f15a44fc8
RiyadHassen <riyadworku@gmail.com>
2020-01-05 06:08:28 +0300

    Asp Mvc with model , view , controller and Migrations

---
+++ Models/Data/MovieContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+
+namespace MvcMovie.Models.Data
+{
+    public class MovieContext : DbContext
+    {
+
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
